6f3591aaaa2495ca42c6fd71a27bfa3efb373d64
noprom <noprom@nopromdeMacBook-Pro.local>
2015-06-16 05:16:42 +0800

    常量和变量

--- MyFirstSwiftPro.playground/Contents.swift
+++ MyFirstSwiftPro.playground/Contents.swift
@@ -3,3 +3,48 @@
 import UIKit
 
 var str = "Hello, playground"
+println("Hello Swift")
+
+/*
+ * 1 常亮和类型
+ *
+ */
+
+let maxNum = 1000
+var index = 0
+
+//maxNum = 100
+
+index = 2
+
+var x = 0.0 , y = 0.0 ,z = 0.0
+
+// type safety
+
+var webSiteName:String
+webSiteName = "www.baidu.com"
+
+var red, green, blue:Double
+
+// 不同的进制
+let decinalInt:Int = 17
+let binaryInt:Int = 0b10001
+let octalInt:Int = 0o21
+let hexadecimalInt:Int = 0x11
+
+let float_a = 0.012
+let float_b = 1.2e-2
+
+let bignum_a = 1000000
+let bignum_b = 1_000_000
+let bignum_c = 1_0000_0000
+
+let num_a:Float = 1
+//let num_b:Int = 1.2
+
+let a:Int = 3
+let b:Double = 0.1415926
+let pi:Double = Double(a)+b
+
+let 姓名 ＝ "李小龙"
+姓名+="你好"
