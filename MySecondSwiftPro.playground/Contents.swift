defe7c615fdb24f260d3ab5d9358f7e0b2c14871
noprom <noprom@nopromdeMacBook-Pro.local>
2015-06-16 05:29:52 +0800

    add second pro

---
+++ MySecondSwiftPro.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+/*
+ * 2 布尔类型
+ */
+
+let imtrueVar = true
+let imfalseVar = false
+
+// 选择语句
+if imfalseVar{
+    println("It's ture!")
+}else if 3+4 == 7{
+    println("3+4=7!")
+}
+else{
+    println("It's false")
+}
+
+var a:Int = 1
+if a == 1{
+    println("a = 1")
+}
