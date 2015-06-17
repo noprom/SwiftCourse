432bb792d4b9f7c5b405829210e132428351dd74
noprom <tyee.noprom@qq.com>
2015-06-17 22:41:04 +0800

    add optionals

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+// 可选值Optional,若没有初始化则没有默认的值
+
+var a:Int
+a ＝ 1
+a
+
+
+var imOptionalVar:Int?
+imOptionalVar = 12
+
+let userInput = "abc"
+var age = userInput.toInt()
+
+if(age != nil){
+    println("骚年你的输入时正确的！并且你的年龄是\(age)");
+}
