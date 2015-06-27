b6bde047b60cc5d3f2ef33e8cdf6ea510501905f
noprom <tyee.noprom@qq.com>
2015-06-27 13:48:03 +0800

    add a corner label

--- GettingStartedWithSwift.playground/Contents.swift
+++ GettingStartedWithSwift.playground/Contents.swift
@@ -5,3 +5,25 @@ import UIKit
 var str = "Hello, playground"
 
 
+var msg1 = "Hello Swift,How can I get started?"
+var msg2 = "The best way to get started is to stop talking and code."
+
+msg1.uppercaseString
+msg2.lowercaseString + "Okay , I am working on it now😀"
+
+if(msg1 == msg2){
+    println("Same msg")
+}else{
+    println("Not the same msg")
+}
+
+let msgLabel = UILabel(frame: CGRectMake(0, 0, 300, 50))
+msgLabel.text = msg1
+msgLabel
+
+// SET THE COLOR AND OTHER THINGS
+msgLabel.backgroundColor = UIColor.orangeColor()
+msgLabel.textAlignment = NSTextAlignment.Center
+msgLabel.layer.cornerRadius = 10.0
+msgLabel.clipsToBounds = true
+msgLabel
