558e86a4b9382c3929fc68d1a50fc2a708900765
noprom <tyee.noprom@qq.com>
2015-07-02 14:43:51 +0800

    字符串的插入值

---
+++ StringBasicOperator.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// 字符串的基本操作
+var str = "Hello, playground"
+
+str += "你们在干啥呢"
+
+// Character 不能+= 只能append
+
+let ch:Character = "!"
+str.append(ch)
+//str+=ch
+
+// 字符串的插入值
+let imInt:Int = 2
+let imDouble:Double = 3.1415926
+let imBool:Bool = true
+let imString:String = "hello"
+let imTuple:(Int,Int) = (2,4)
+let imOptional:Int? = nil
+let imCharacter:Character = "!"
+
+println("Int:\(imInt)")
+println("Int:\(imDouble)")
+println("Int:\(imBool)")
+println("Int:\(imString)")
+println("Int:\(imTuple)")
+println("Int:\(imOptional)")
+println("Int:\(imCharacter)")
+
