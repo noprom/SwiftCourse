fd2474bf5feadac43019b537dcf0c722f317744b
noprom <tyee.noprom@qq.com>
2015-07-02 13:35:42 +0800

    for in 0...9

--- RangeOperator.playground/Contents.swift
+++ RangeOperator.playground/Contents.swift
@@ -6,6 +6,15 @@ import UIKit
 // [a,b] a...b
 // [a,b) a..<b
 
-for index in 1...9{
+for index in 1...10{
     index
+}
+
+for index in 1..<10{
+    index
+}
+
+let courseNames = ["你是谁啊", "我怎么知道呢", "那你说你是谁"]
+for i in 0..<courseNames.count{
+    courseNames[i]
 }
