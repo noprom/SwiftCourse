aad2f4a5342f17d32164336bbfa79d5be6b5e024
noprom <tyee.noprom@qq.com>
2015-07-03 14:41:38 +0800

    clear the dictionary

---
+++ Dictionary.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+// 字典
+var dic:[Int:String] = [1:"A", 2:"B", 3:"C"]
+dic[1]
+
+var dic2:[String:String] = ["A":"你们","B":"我靠"]
+dic2["A"]
+
+var dic3:Dictionary<Int,String> = [99:"AAA",32:"BBB"]
+dic3[99]
+
+// 空字典
+var dic4 = Dictionary<Int,String>()
+var dic5 = Dictionary<String,String>()
+var dic6 = [Int:String]()
+
+// 清空字典
+dic3 = [Int:String]()
+
