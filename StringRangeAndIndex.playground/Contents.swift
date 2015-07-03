86ee18b93f069e39f09f70b0f7ff5d4bd03a2a12
noprom <tyee.noprom@qq.com>
2015-07-03 13:54:55 +0800

    replace string

--- StringRangeAndIndex.playground/Contents.swift
+++ StringRangeAndIndex.playground/Contents.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 
-var str = "Hello, playground~play your sister"
+var str = "Hello, playground~play your sister!"
 
 // Range
 str.rangeOfString("play")
@@ -24,6 +24,37 @@ str.rangeOfString("play", options: NSStringCompareOptions.CaseInsensitiveSearch,
 // substring
 var toIndex = advance(str.startIndex, 4)
 str.substringToIndex(toIndex)
-str.substringFromIndex(str.startIndex)
+
+var fromIndex = advance(str.startIndex, 14)
+str.substringFromIndex(fromIndex)
+
+// substringWithRange
+str.substringWithRange(Range<String.Index>(start:toIndex,end:fromIndex))
+
+// insert
+var insertIndex = advance(str.startIndex, 3)
+str.insert("!", atIndex: insertIndex)
+
+// remove
+str.removeAtIndex(insertIndex)
+str.removeRange(Range<String.Index>(start: str.startIndex, end: insertIndex))
+
+// replace
+var replaceEndIndex = advance(str.startIndex, 5)
+str.stringByReplacingCharactersInRange(Range<String.Index>(start: str.startIndex, end: replaceEndIndex), withString: "你妹")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
