cba4b04c063dc018a287ccca3c48ec9b18e3743b
noprom <tyee.noprom@qq.com>
2015-07-03 14:19:05 +0800

    usage of array

---
+++ UsageOfArray.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+// 数组的基本使用
+var arr = ["A", "B", "C", "D", "E"]
+arr.count
+arr.isEmpty
+
+
+arr.append("X")
+arr += ["OOOOO"]
+arr += ["1","2","3"]
+
+// insert
+arr.insert("PPP", atIndex: 0)
+
+// remove
+arr.removeAtIndex(2)
+arr.removeLast()
+arr
+//arr.removeAll(keepCapacity: false)
+
+//arr.insert("~~~", atIndex: 200)
