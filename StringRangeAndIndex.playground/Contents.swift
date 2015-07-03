aa8501b42e468ecdd444eae7695b3b93ab7f3507
noprom <tyee.noprom@qq.com>
2015-07-03 13:43:17 +0800

    Range.Index done

--- StringRangeAndIndex.playground/Contents.swift
+++ StringRangeAndIndex.playground/Contents.swift
@@ -7,6 +7,19 @@ var str = "Hello, playground~play your sister"
 // Range
 str.rangeOfString("play")
 str.rangeOfString("play", options: NSStringCompareOptions.BackwardsSearch)
+str.rangeOfString("Play", options: NSStringCompareOptions.CaseInsensitiveSearch)
+
+// Index
+str.startIndex
+str.endIndex
+let strRange = Range<String.Index>(start: str.startIndex, end: str.endIndex)
+
+// Range.Index
+let startIndex:String.Index = str.startIndex
+let endIndex:String.Index = advance(startIndex, 10)
+let searchRange = Range<String.Index>(start:startIndex,end:endIndex)
+str.rangeOfString("play", options: NSStringCompareOptions.CaseInsensitiveSearch, range: searchRange)
+
 
 
 
