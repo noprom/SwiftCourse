65891b86d6e23939235813493aa6f37bfa0d33d5
noprom <tyee.noprom@qq.com>
2015-07-05 13:51:24 +0800

    Usage of Dictionary

---
+++ UsageOfDictionary.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// 字典的使用
+var courses = [76:"你好啊",32:"不得哦",10:"那是哦"]
+var websites = ["搜索引擎":"百度","学习网站":"慕课网"]
+
+courses.count
+courses.isEmpty
+
+
