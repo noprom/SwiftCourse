4b527554b9d3909f0c77f25667a1bfecc00314dc
noprom <tyee.noprom@qq.com>
2015-07-05 14:02:28 +0800

    bianli dictionary

--- UsageOfDictionary.playground/Contents.swift
+++ UsageOfDictionary.playground/Contents.swift
@@ -3,10 +3,37 @@
 import UIKit
 
 // 字典的使用
-var courses = [76:"你好啊",32:"不得哦",10:"那是哦"]
+var courses = [-76:"你好啊",32:"不得哦",10:"那是哦"]
 var websites = ["搜索引擎":"百度","学习网站":"慕课网"]
 
 courses.count
 courses.isEmpty
 
+// Optional
+courses[76]
+courses[-76]
+websites["搜索引擎"]
+websites["你妹"]
 
+"课程名字" + courses[-76]!
+courses[73] = "你好哦"
+courses
+
+courses[73] = "不得哦"
+courses
+
+courses.updateValue("2048", forKey: -76)
+courses
+
+// 删除字典的值
+courses[32] = nil
+courses
+
+
+courses.removeValueForKey(10)
+courses
+
+// 字典的遍历
+for (key,val) in courses{
+    println("key = \(key) , value = \(val)")
+}
